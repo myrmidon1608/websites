2cf18fd52949a39a47fe51a88139f59e7b4228a8
Nicholas Moore <myrmidon16@gmail.com>
2014-08-26 23:38:39 -0400

    RFM Google Analytics

--- riverview-farmers-market/wp-content/themes/rfm/header.php
+++ riverview-farmers-market/wp-content/themes/rfm/header.php
@@ -13,6 +13,17 @@
         <link rel="stylesheet" href="<?php root(); ?>/core/css/fonts.css" />
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+        
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-54093348-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        
         <?php wp_head(); ?>
     </head>
     <body>
